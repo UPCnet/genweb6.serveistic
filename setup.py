4fd795bce1b35a9b9595b7bf5df8642b9877c3e2
Pilar Marinas <pilar.marinas@upcnet.es>
2023-09-12 08:21:21 +0200

    Preparing release 1.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='genweb6.serveistic',
-    version='1.0a1',
+    version='1.0',
     description=" Genweb 6 Serveistic package",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
