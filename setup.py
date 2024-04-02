71908b9fdd8f10c2cb38a68df1353f31ae95a9b1
Pilar Marinas <pilar.marinas@upcnet.es>
2024-04-02 09:18:03 +0200

    Back to development: 1.14

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='genweb6.serveistic',
-    version='1.13',
+    version='1.14.dev0',
     description=" Genweb 6 Serveistic package",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
