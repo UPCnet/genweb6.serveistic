afe9b8423f394c1749c5630977a1311335cc86e4
Iago López <iago.lopez@upcnet.es>
2026-04-21 09:22:34 +0200

    Back to development: 1.38

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='genweb6.serveistic',
-    version='1.37',
+    version='1.38.dev0',
     description=" Genweb 6 Serveistic package",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
