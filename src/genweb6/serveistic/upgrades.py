713b81c214c350d1900a5e1dbc5246e343e4adb2
Iago López <iago.lopez@upcnet.es>
2026-06-23 13:52:08 +0200

    [UPD] Traducciones

--- src/genweb6/serveistic/upgrades.py
+++ src/genweb6/serveistic/upgrades.py
@@ -1,28 +1,12 @@
 # -*- coding: utf-8 -*-
-from plone import api
-
-from genweb6.serveistic.setuphandlers import reindex_servei_facetas
+from Products.CMFPlone.utils import get_installer
 
 import logging
-import transaction
-
 
 logger = logging.getLogger(__name__)
 
-PROFILE_ID = 'profile-genweb6.serveistic:default'
 
-IMPORT_STEPS = (
-    'catalog',
-    'plone.app.registry',
-    'genweb6.serveistic.various',
-)
-
-
-def upgrade_import_profile(context):
-    """Reimport serveistic profile steps and reindex servei_facetas."""
-    setup = api.portal.get_tool(name='portal_setup')
-    for step in IMPORT_STEPS:
-        logger.info('Running import step %s for %s', step, PROFILE_ID)
-        setup.runImportStepFromProfile(PROFILE_ID, step)
-    reindex_servei_facetas()
-    transaction.commit()
+def upgrade_by_reinstall(context):
+    qi = get_installer(context)
+    qi.uninstall_product("genweb6.serveistic")
+    qi.install_product("genweb6.serveistic")
